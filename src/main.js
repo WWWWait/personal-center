f1000e22699c833f948fe97d943a5a42ff0df9ea
不并世 <202854630@qq.com>
2021-08-30 15:25:58 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+// 项目入口
+
 import { createApp } from 'vue'
+
 import App from './App.vue'
+
 import router from './router'
+
 import store from './store'
+
+//加载全局样式文件
+import './styles/index.less'
 
 createApp(App).use(store).use(router).mount('#app')
